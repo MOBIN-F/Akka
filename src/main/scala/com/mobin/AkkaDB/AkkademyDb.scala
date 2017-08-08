0bb3be37c014f659731d721b2b2729f5f963663c
mobin <996833461@qq.com>
2017-08-08 22:43:52 +0800

    AkkaaDb

--- src/main/scala/com/mobin/AkkaDB/AkkademyDb.scala
+++ src/main/scala/com/mobin/AkkaDB/AkkademyDb.scala
@@ -1,6 +1,6 @@
 package com.mobin.AkkaDB
 
-import akka.actor.Actor
+import akka.actor.{Props, ActorSystem, Status, Actor}
 import akka.event.Logging
 
 import scala.collection.mutable.HashMap
@@ -9,6 +9,8 @@ import scala.collection.mutable.HashMap
   * Created by Mobin on 2017/8/6.
   */
 case class SetRequest(key: String, value: Object)
+case class GetRequest(key: String)
+case class KeyNotFoundException(key: String) extends Exception   //定义异常类
 
 class AkkademyDb extends Actor{
   val map = new HashMap[String, Object]
@@ -18,7 +20,21 @@ class AkkademyDb extends Actor{
     case SetRequest(key, value) => {
       log.info("received SetRequest - key: {} value : {}", key,value)
       map.put(key ,value)
+      sender() ! Status.Success
     }
-    case o => log.info("received unknow message: {}", o)
+    case GetRequest(key) => {
+      log.info("received GetRequest - key: {}", key)
+      val response = map.get(key)
+      response match {
+        case Some(x) => sender() ! x     //找到key就返回
+        case None => sender()! Status.Failure(new KeyNotFoundException(key))   //否则抛出异常
+      }
+    }
+    case o => Status.Failure(new ClassNotFoundException)
   }
+
+}
+object Main extends App{
+  val system = ActorSystem("akkademy")
+  system.actorOf(Props[AkkademyDb], "akkademy-db")
 }
